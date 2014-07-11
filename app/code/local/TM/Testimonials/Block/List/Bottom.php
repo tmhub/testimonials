45436d9415013bc03a10927f3f4a6179545c3ccb
Roman Chumak <chumakrom@gmail.com>
2014-07-11 13:34:59 +0300

    Added full testimonials list widget

--- app/code/local/TM/Testimonials/Block/List/Bottom.php
+++ app/code/local/TM/Testimonials/Block/List/Bottom.php
@@ -4,10 +4,20 @@ class TM_Testimonials_Block_List_Bottom extends Mage_Core_Block_Template
 {
     protected function _beforeToHtml()
     {
-        $testimonials = $this->getLayout()
-            ->getBlock('testimonials.list.content')
-            ->getTestimonials();
-        
+        if ($this->getLayout()->getBlock('testimonials.list.content')) {
+            $testimonials = $this->getLayout()
+                ->getBlock('testimonials.list.content')
+                ->getTestimonials();
+        } else {
+            $testimonials = Mage::getModel('tm_testimonials/data')->getCollection()
+                ->addFieldToSelect('*')
+                ->addStoreFilter(Mage::app()->getStore())
+                ->addFieldToFilter('status', TM_Testimonials_Model_Data::STATUS_ENABLED)
+                ->setOrder('date', 'desc')
+                ->setPageSize($this->getPerPage())
+                ->setCurPage($this->getCurrentPage());
+        }
+
         $this->setTestimonials($testimonials);
         return parent::_beforeToHtml();
     }
---
+++ app/code/local/TM/Testimonials/Block/Widget/ListFull.php
@@ -0,0 +1,59 @@
+<?php
+
+class TM_Testimonials_Block_Widget_ListFull extends TM_Testimonials_Block_List_Content
+    implements Mage_Widget_Block_Interface
+{
+    /**
+     * Initialize block's cache
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+
+        $this->addData(array(
+            'cache_lifetime' => false,
+            'cache_tags'     => array(Mage_Core_Model_Store::CACHE_TAG, Mage_Cms_Model_Block::CACHE_TAG)
+        ));
+    }
+
+    /**
+     * Get Key pieces for caching block content
+     *
+     * @return array
+     */
+    public function getCacheKeyInfo()
+    {
+        return array(
+            'TM_TESTIMONIALS_WIDGET_LISTFULL',
+            Mage::app()->getStore()->getId(),
+            (int)Mage::app()->getStore()->isCurrentlySecure(),
+            Mage::getDesign()->getPackageName(),
+            Mage::getDesign()->getTheme('template'),
+            $this->getTemplate()
+        );
+    }
+
+    public function getTemplate()
+    {
+        $template = parent::getTemplate();
+        if (!$template) {
+            $template = 'tm/testimonials/list/content.phtml';
+        }
+        return $template;
+    }
+
+    protected function _afterToHtml($html)
+    {
+        $titleBlock = $this->getLayout()
+            ->createBlock('testimonials/list_title')
+            ->setTemplate('tm/testimonials/list/title.phtml')
+            ->toHtml();
+
+        $bottomBlock = $this->getLayout()
+            ->createBlock('testimonials/list_bottom')
+            ->setTemplate('tm/testimonials/list/bottom.phtml')
+            ->toHtml();
+
+        return $titleBlock . $html . $bottomBlock;
+    }
+}
